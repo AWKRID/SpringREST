f07ff7638b4a6b22fd1ed29a6de90b087be7d0bd
AWKRID <minzoo202738@gmail.com>
2024-05-08 15:12:44 +0900

    :sparkles: feat: Repository Implementation
    Connect with supabase DB
    Implement each repository
    Connect service layer with repository layer

---
+++ src/main/kotlin/com/teamsparta/courseregistration/domain/courseapplication/repository/CourseApplicationRepository.kt
@@ -0,0 +1,10 @@
+package com.teamsparta.courseregistration.domain.courseapplication.repository
+
+import com.teamsparta.courseregistration.domain.courseapplication.model.CourseApplication
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CourseApplicationRepository: JpaRepository<CourseApplication, Long> {
+    fun existsByCourseIdAndUserId(courseId: Long, userId: Long): Boolean
+
+    fun findByCourseIdAndId(courseId: Long, userId: Long): CourseApplication?
+}
---
+++ src/main/kotlin/com/teamsparta/courseregistration/domain/lecture/repository/LectureRepository.kt
@@ -0,0 +1,8 @@
+package com.teamsparta.courseregistration.domain.lecture.repository
+
+import com.teamsparta.courseregistration.domain.lecture.model.Lecture
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface LectureRepository: JpaRepository<Lecture, Long> {
+    fun findByCourseIdAndId(courseId: Long, id: Long): Lecture?
+}
